daecd497053247cc9b337fb380c1656482111e7a
1 <Anasko162@Mail.ru>
2022-11-06 20:23:13 +0300

    Задача. Всё еще думаю над её решением с третьим условием Human age >=24

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int HumanAge = 10;
+         boolean Vozvrast = HumanAge>=18 && HumanAge<=24;
+         boolean VozvrasCheloveka = HumanAge>=7 && HumanAge<=18;
+     if (VozvrasCheloveka)
+        System.out.println("Вы ходите в школу");
+        else {
+        System.out.println("Вы учитесь в университете"); }
+    }
+} //Не поняла как добавить третье условие для HumanAge >=24   System.out.println("вам пора на работу");
