73a51bb65b185b5f1a05a848001e2de75fb6047d
1 <Anasko162@Mail.ru>
2022-11-06 18:04:31 +0300

    Домашние задание -2. 1 задача.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!"); {
+    }
+    int HumanAges = 7;
+    if (HumanAges >=18) {
+        System.out.print("Вы достигли совершеннолетия,Поздравляем!");
+    }
+        else {
+            System.out.print("Возраст вашего совершеннолетия еще не наступил, просим еще немного подождать. ");
+        }
+    }
+    }
+
